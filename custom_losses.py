1ce2424d1b2ba98dddd0f4e913d7f4c0862f8a1a
Emi-Lo <emiliano.longo@polimi.it>
2026-01-13 18:47:16 +0100

    Update custom_losses.py adding QL, EL, WQE

--- custom_losses.py
+++ custom_losses.py
@@ -1,13 +1,16 @@
 import tensorflow as tf
 
-
-def quantile_loss_huber(y_true, y_pred, quantile, alpha=0.1):
-    errors = y_true - y_pred
-    abs_errors = tf.abs(errors)
-    huber_errors = tf.where(abs_errors <= alpha, 0.5 * tf.square(errors)/alpha, (abs_errors - 0.5 * alpha))
-    quantile_loss = tf.maximum(quantile * huber_errors * (y_true - y_pred), (1-quantile) * huber_errors * (y_pred - y_true))
+def quantile_loss(y_true, y_pred, quantile=0.25):
+    quantile_loss = tf.maximum(quantile * (y_true - y_pred), (1-quantile) * (y_pred - y_true))
     return tf.reduce_mean(quantile_loss)
-
-def quantile_loss(y_true, y_pred):
-    composite_loss = tf.reduce_mean([quantile_loss_huber(y_true, y_pred, 0.90)])
+    
+def expectile_loss(y_true, y_pred, expectile=0.82):
+    errors = y_true - y_pred
+    quantile_loss = tf.where(errors >=0, expectile * tf.square(errors), (1-expectile) * tf.square(errors))
+    return tf.reduce_mean(quantile_loss)
+    
+def weighted_quantile_expectile(y_true, y_pred):
+    composite_loss = tf.add((1/6)*quantile_loss(y_true, y_pred, 0.25),
+                            (5/6)*expectile_loss(y_true, y_pred, 0.82))
     return composite_loss
+    
